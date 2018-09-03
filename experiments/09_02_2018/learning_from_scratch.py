d1e26c63f926880f43caf2245a76e5caf860b812
rodrigoazs <rodrigo4zevedo@gmail.com>
2018-09-03 17:50:58 -0300

    Sent transfer and learning from scratch experiments

--- experiments/09_02_2018/learning_from_scratch.py
+++ experiments/09_02_2018/learning_from_scratch.py
@@ -21,18 +21,17 @@ import random
 import json
 
 firstRun = False
-validation_size = 0.2
 n_runs = 1
 
-if os.path.isfile('transfer_experiment.json'):
-    with open('transfer_experiment.json', 'r') as fp:
+if os.path.isfile('learning_from_scratch.json'):
+    with open('learning_from_scratch.json', 'r') as fp:
         results = json.load(fp)
 else:
     results = { 'results': {}, 'save': {}}
     firstRun = True
 
 def save(data):
-    with open('transfer_experiment.json', 'w') as fp:
+    with open('learning_from_scratch.json', 'w') as fp:
         json.dump(data, fp)
         
 if firstRun:
@@ -132,94 +131,41 @@ cora_bk = ['sameauthor(+author,+author).',
           'haswordvenue(+venue,-word).',
           'haswordvenue(-venue,+word).']
 
-experiments = ['imdb_to_uwcse', 'imdb_to_cora', 'uwcse_to_imdb', 'uwcse_to_cora']
+experiments = ['imdb', 'uwcse', 'cora']
 start = time.time()
 
 while results['save']['experiment'] < len(experiments) and results['save']['run'] < n_runs:
     experiment = experiments[results['save']['experiment']]
     if experiment not in results['results']:
         results['results'][experiment] = []
-    # imdb to uwcse
-    if experiment == 'imdb_to_uwcse':
-        source_target = imdb_target
-        source_facts, source_pos, source_neg = imdb_facts, imdb_pos, imdb_neg
-        source_bk = imdb_bk
-        target_target = uwcse_target
-        target_facts, target_pos, target_neg = uwcse_facts, uwcse_pos, uwcse_neg
-        target_bk = uwcse_bk
-        mapping = ['workedunder(A, B) -> advisedby(A, B)',
-                'director(A) -> professor(A)',
-                'actor(A) -> student(A)',
-                'movie(A, B) -> publication(A, B)',
-                'female(A) -> student(A)'
-                ]      
-    # imdb to cora
-    elif experiment == 'imdb_to_cora':
-        source_target = imdb_target
-        source_facts, source_pos, source_neg = imdb_facts, imdb_pos, imdb_neg
-        source_bk = imdb_bk
-        target_target = cora_target
-        target_facts, target_pos, target_neg = cora_facts, cora_pos, cora_neg
-        target_bk = cora_bk
-        mapping = ['workedunder(A, B) -> sameauthor(A, B)',
-                'movie(A, B) -> author(A, B)',
-                'genre(A, B) -> haswordauthor(B, A)'
-                ] 
-    # uwcse to imdb
-    elif experiment == 'uwcse_to_imdb':
-        source_target = uwcse_target
-        source_facts, source_pos, source_neg = uwcse_facts, uwcse_pos, uwcse_neg
-        source_bk = uwcse_bk
-        target_target = imdb_target
-        target_facts, target_pos, target_neg = imdb_facts, imdb_pos, imdb_neg
-        target_bk = imdb_bk
-        mapping = ['advisedby(A, B) -> workedunder(A, B)',
-                'professor(A) -> director(A)',
-                'student(A) -> actor(A)',
-                'publication(A, B) -> movie(A, B)',
-                'student(A) -> female(A)'
-                ]    
-    # uwcse to cora
-    elif experiment == 'uwcse_to_cora':
-        source_target = uwcse_target
-        source_facts, source_pos, source_neg = uwcse_facts, uwcse_pos, uwcse_neg
-        source_bk = uwcse_bk
-        target_target = cora_target
-        target_facts, target_pos, target_neg = cora_facts, cora_pos, cora_neg
-        target_bk = cora_bk
-        mapping = ['advisedby(A, B) -> sameauthor(A, B)',
-                'publication(A, B) -> author(A, B)',
-                ]   
-    # cora to imdb
-    #elif experiment == 'cora_to_imdb':
-    # cora to uwcse
-    #elif experiment == 'cora_to_uwcse':    
+    # imdb
+    if experiment == 'imdb':
+        target = imdb_target
+        facts, pos, neg = imdb_facts, imdb_pos, imdb_neg
+        bk = imdb_bk    
+    # uwcse
+    elif experiment == 'uwcse':
+        target = uwcse_target
+        facts, pos, neg = uwcse_facts, uwcse_pos, uwcse_neg
+        bk = uwcse_bk
+    # cora
+    elif experiment == 'cora':
+        target = cora_target
+        facts, pos, neg = cora_facts, cora_pos, cora_neg
+        bk = cora_bk  
     
-    background = boostsrl.modes(source_bk, [source_target], useStdLogicVariables=False, maxTreeDepth=12, nodeSize=3, numOfClauses=12)
+    background = boostsrl.modes(bk, [target], useStdLogicVariables=False, maxTreeDepth=12, nodeSize=3, numOfClauses=12)
     
     # group source folds
-    source_pos = group_folds(source_pos)
-    source_neg = group_folds(source_neg)
-    source_facts = group_folds(source_facts)
-    
-    # shuffle all examples
-    random.shuffle(source_pos)
-    random.shuffle(source_neg)
-    source_neg = source_neg[:len(source_pos)]
+    #pos = group_folds(pos)
+    #neg = group_folds(neg)
+    #facts = group_folds(facts)
       
-    # learning from source
-    [model, total_revision_time, structured, will] = learn_model(background, boostsrl, source_target, source_pos, source_neg, source_facts, refine=None, trees=10, verbose=False)
-    
-    # transfer
-    transferred_structured = transfer(structured, mapping)
-    
-    background = boostsrl.modes(target_bk, [target_target], useStdLogicVariables=False, maxTreeDepth=12, nodeSize=3, numOfClauses=12)
-    
-    n_folds = len(target_pos)
+    n_folds = len(pos)
     for i in range(n_folds):
-        train_pos, test_pos = get_kfold(i, target_pos)
-        train_neg, test_neg = get_kfold(i, target_neg)
-        train_facts, test_facts = get_kfold(i, target_facts)
+        train_pos, test_pos = get_kfold(i, pos)
+        train_neg, test_neg = get_kfold(i, neg)
+        train_facts, test_facts = get_kfold(i, facts)
         
         # shuffle all examples
         #random.shuffle(train_pos)
@@ -228,18 +174,10 @@ while results['save']['experiment'] < len(experiments) and results['save']['run'
         train_neg = train_neg[:len(train_pos)]
         test_neg = test_neg[:len(test_pos)]
         
-        # separate train and validation    
-        validation_pos = train_pos[:int(validation_size*len(train_pos))]
-        validation_neg = train_neg[:int(validation_size*len(train_neg))]
-        train_pos = train_pos[int(validation_size*len(train_pos)):]
-        train_neg = train_neg[int(validation_size*len(train_neg)):]
-        
         # transfer and revision
-        [model, total_revision_time, inference_time, t_results, structured, pl_inference_time, pl_t_results] = theory_revision(background, boostsrl, target_target, train_pos, train_neg, train_facts, validation_pos, validation_neg, test_pos, test_neg, test_facts, 1.0, transferred_structured, trees=10, max_revision_iterations=10, verbose=False, testAfterPL=True)
-        t_results['Learning time'] = total_revision_time
+        [model, learning_time, inference_time, t_results, structured, will] = learn_test_model(background, boostsrl, target, train_pos, train_neg, train_facts, test_pos, test_neg, test_facts, refine=None, trees=10, verbose=False)
+        t_results['Learning time'] = learning_time
         t_results['Inference time'] = inference_time
-        t_results['Parameter Learning time'] = pl_inference_time
-        t_results['Parameter Learning results'] = pl_t_results
         results['results'][experiment].append(t_results)
         print('Dataset: %s, Fold: %s, Time: %s' % (experiment, i+1, time.strftime('%H:%M:%S', time.gmtime(time.time()-start))))
         print(t_results)
