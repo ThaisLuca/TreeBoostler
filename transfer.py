ba479596aef5da975045ab4e3458494163dd5dcd
rodrigoazs <rodrigo4zevedo@gmail.com>
2018-10-31 08:45:01 -0300

    Modified algorithms

---
+++ transfer.py
@@ -0,0 +1,194 @@
+'''
+   Functions to handle transfer learning of boosted trees
+   Name:         transfer.py
+   Author:       Rodrigo Azevedo
+   Updated:      August 31, 2018
+   License:      GPLv3
+'''
+#from revision import *
+import copy
+import re
+
+#transfer_map = ['workedunder(A, B) -> advisedby(B, A)',
+#            'director(A) -> professor(A)',
+#            'actor(A) -> student(A)',
+#            'movie(A, B) -> publication(B, B)'
+#            ]
+#
+#structured = [['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)',
+#   'false': 'teste(A,B)',
+#   'false,false': 'movie(B,A)'},
+#  {'false,true': [6.83e-08, 77, 0], 'false,false,false': [6.83e-08, 77, 0], 'false,false,true': [6.83e-08, 77, 0], 'true': [0.0, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [0.0, 77, 0], 'true': [2.23e-07, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [5.67e-08, 77, 0], 'true': [3.26e-07, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [5.27e-08, 77, 0], 'true': [0.0, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [0.0, 77, 0], 'true': [0.0, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [1.83e-08, 77, 0], 'true': [0.0, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [3.57e-08, 77, 0], 'true': [0.0, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [0.0, 77, 0], 'true': [2.98e-08, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [2.89e-08, 77, 0], 'true': [7.88e-08, 0, 77]}],
+# ['workedunder(A, B)',
+#  {'': 'director(B), movie(C, A), movie(C, B)'},
+#  {'false': [0.0, 77, 0], 'true': [5.37e-08, 0, 77]}]]
+
+class transfer:
+    def literal_to_str(literal):
+        '''Generate literal string from tuple'''
+        return literal[0] + '(' + literal[1] + ')'
+        
+    def transfer_variables(variables, from_map, to_map):
+        '''Transfer variables of a clause given a mapping'''
+        read = {}
+        ret = []
+        values = variables.split(',')
+        f = from_map.split(',')
+        t = to_map.split(',')
+        for i in range(len(values)):
+            read[f[i].strip()] = values[i].strip()
+        for i in range(len(t)):
+            ret.append(read[t[i].strip()])
+        return ', '.join(ret)
+        
+    def get_mapping_struct(mapping):
+        '''Generate mapping structure'''
+        ret = {}
+        for m in mapping:
+            match = re.findall('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', m)
+            if match:
+                ret[match[0][0]] = (match[1][0], match[0][1], match[1][1])
+        return ret
+        
+    def transfer_literal(literal, mapping_struct):
+        if literal[0] in mapping_struct:
+            m = mapping_struct[literal[0]]
+            return (m[0], transfer.transfer_variables(literal[1], m[1], m[2]))
+        else:
+            return None
+            
+    def get_transfer_tree_helper(path, nodes, leaves):
+        children = [None, None]
+        split = [] if path == '' else path.split(',')
+        left = ','.join(split+['true'])
+        right = ','.join(split+['false'])
+        if left in nodes:
+            children[0] = transfer.get_transfer_tree_helper(left, nodes, leaves)
+        if right in nodes:
+            children[1] = transfer.get_transfer_tree_helper(right, nodes, leaves)
+        if left in leaves:
+            children[0] = leaves[left] # { 'type': 'leaf', 'std_dev': leaves[left][0], 'neg': leaves[left][1], 'pos': leaves[left][2] } 
+        if right in leaves:
+            children[1] = leaves[right]
+        return { nodes[path]: children }
+        # { 'type': 'node', 'literals': nodes[path], 'children': children, 'variavarc] }
+        
+    def get_transfer_tree(nodes, leaves):
+        return transfer.get_transfer_tree_helper('', nodes, leaves)
+        
+    def transfer_tree_helper(root, mapping_struct):
+        if isinstance(root, list):
+            return root
+        elif isinstance(root, dict):
+            node_str = list(root.keys())[0]
+            value = root[node_str]
+            children = value
+            true_child = transfer.transfer_tree_helper(children[0], mapping_struct)
+            false_child = transfer.transfer_tree_helper(children[1], mapping_struct)
+            # nodes with no literals should be replaced by its false subtree
+            match = re.findall('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', node_str)
+            new_clause = []
+            if match:
+                for m in match:
+                    transfered = transfer.transfer_literal(m, mapping_struct)
+                    if transfered:
+                        new_clause.append(transfer.literal_to_str(transfered))
+            if len(new_clause):
+                new_key = ', '.join(new_clause)
+                return { new_key: [true_child, false_child] }
+            else:
+                return false_child  
+        
+    def transfer_tree(tree, mapping_struct):
+        ntree = copy.deepcopy(tree)
+        return transfer.transfer_tree_helper(ntree, mapping_struct)
+        
+    def get_structured_from_transfer_tree_helper(path, root, nodes, leaves):
+        if isinstance(root, list):
+            leaves[path] = root
+        elif isinstance(root, dict):
+            i = list(root.keys())[0]
+            value = root[i]
+            children = value
+            split = [] if path == '' else path.split(',')
+            left = ','.join(split+['true'])
+            right = ','.join(split+['false'])
+            nodes[path] = i
+            transfer.get_structured_from_transfer_tree_helper(left, children[0], nodes, leaves)
+            transfer.get_structured_from_transfer_tree_helper(right, children[1], nodes, leaves)
+        
+    def get_structured_from_transfer_tree(target, tree):
+        nodes = {}
+        leaves = {}
+        transfer.get_structured_from_transfer_tree_helper('', tree, nodes, leaves)
+        return [target, nodes, leaves]
+        
+    def transfer(structured, mapping):
+        '''Transfer structure according to mapping'''
+        copied = copy.deepcopy(structured)
+        mapping_struct = transfer.get_mapping_struct(mapping)
+        for struct in copied:
+            match = re.match('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', struct[0])
+            if match:
+                transfered = transfer.transfer_literal(match.groups(), mapping_struct)
+                if transfered:
+                    struct[0] = transfer.literal_to_str(transfered)
+                else:
+                    raise(Exception('Attempted to transfer head to a None mapping.'))
+            else:
+                 raise(Exception('Attempted to transfer head that does not exist.'))
+            # nodes with no literals should be replaced by its subtree
+            tree = transfer.get_transfer_tree(struct[1], struct[2])
+            transferred = transfer.transfer_tree(tree, mapping_struct)
+            new_struct = transfer.get_structured_from_transfer_tree(struct[0], transferred)
+    #        remove_nodes = []
+    #        new_nodes = {}
+    #        for key, value in struct[1].items():
+    #            if ','.join((key.split(','))[:-1]) not in remove_nodes:
+    #                match = re.findall('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', value)
+    #                new_clause = []
+    #                if match:
+    #                    for m in match:
+    #                        transfered = transfer_literal(m, mapping_struct)
+    #                        if transfered:
+    #                            new_clause.append(literal_to_str(transfered))
+    #                if len(new_clause):
+    #                    #struct[1][key] = ', '.join(new_clause)
+    #                    new_nodes[key] = ', '.join(new_clause)
+    #                else:
+    #                    remove_nodes.append(key)
+            struct[1] = new_struct[1]
+            struct[2] = new_struct[2]
+        return copied
+        
+    def get_transferred_target(structured):
+        '''Remove target from structured tree'''
+        target = structured[0][0]
+        match = re.match('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', target)
+        if match:
+            return match.groups()[0]
---
+++ transfer_experiment.py
@@ -0,0 +1,398 @@
+'''
+   Learning curve experiment
+   Name:         transfer_experiment.py
+   Author:       Rodrigo Azevedo
+   Updated:      July 23, 2018
+   License:      GPLv3
+'''
+
+import os
+#import sys
+import time
+#sys.path.append('../..')
+
+from datasets.get_datasets import *
+from revision import *
+from transfer import *
+from mapping import *
+from boostsrl import boostsrl
+import numpy as np
+import random
+import json
+
+firstRun = False
+verbose = True
+n_runs = 10
+
+    
+#def print(message):
+#    global experiment_title
+#    with open('log/' + experiment_title + '.txt', 'a') as f:
+#        builtins.print(message, file=f)
+#        builtins.print(message)
+
+experiments = [
+            #{'source':'imdb', 'target':'uwcse', 'predicate':'workedunder'},
+            #{'source':'uwcse', 'target':'imdb', 'predicate':'advisedby'},
+            #{'source':'imdb', 'target':'cora', 'predicate':'workedunder'},
+            #{'source':'cora', 'target':'imdb', 'predicate':'samevenue'},
+            {'source':'yeast', 'target':'twitter', 'predicate':'interaction'},
+            {'source':'twitter', 'target':'yeast', 'predicate':'follows'},
+            #{'source':'nell_sports', 'target':'nell_finances', 'predicate':'teamplayssport'},
+            #{'source':'nell_finances', 'target':'nell_sports', 'predicate':'companyeconomicsector'},
+            #{'source':'yeast', 'target':'webkb', 'predicate':'proteinclass'},
+            #{'source':'webkb', 'target':'yeast', 'predicate':'departmentof'},
+            #{'source':'twitter', 'target':'webkb', 'predicate':'accounttype'},
+            #{'source':'webkb', 'target':'twitter', 'predicate':'pageclass'},
+            ]
+            
+bk = {
+      'imdb': ['workedunder(+person,+person).',
+              'workedunder(+person,-person).',
+              'workedunder(-person,+person).',
+              'female(+person).',
+              'actor(+person).',
+              'director(+person).',
+              'movie(+movie,+person).',
+              'movie(+movie,-person).',
+              'movie(-movie,+person).',
+              'genre(+person,+genre).'],
+      'uwcse': ['professor(+person).',
+        'student(+person).',
+        'advisedby(+person,+person).',
+        'advisedby(+person,-person).',
+        'advisedby(-person,+person).',
+        'tempadvisedby(+person,+person).',
+        'tempadvisedby(+person,-person).',
+        'tempadvisedby(-person,+person).',
+        #'ta(+course,+person,+quarter).',
+        #'ta(-course,+person,+quarter).',
+        #'ta(+course,-person,+quarter).',
+        #'ta(+course,+person,-quarter).',
+        #'ta(-course,+person,-quarter).',
+        #'ta(+course,-person,-quarter).',
+        'hasposition(+person,+faculty).',
+        'hasposition(+person,-faculty).',
+        'hasposition(-person,+faculty).',
+        'publication(+title,+person).',
+        'publication(+title,-person).',
+        'publication(-title,+person).',
+        'inphase(+person,+prequals).',
+        'inphase(+person,-prequals).',
+        'inphase(-person,+prequals).',
+        'courselevel(+course,+level).',
+        'courselevel(+course,-level).',
+        'courselevel(-course,+level).',
+        'yearsinprogram(+person,+year).',
+        'yearsinprogram(-person,+year).',
+        'yearsinprogram(+person,-year).',
+        'projectmember(+project,+person).',
+        'projectmember(+project,-person).',
+        'projectmember(-project,+person).'],
+        #'sameproject(project, project).',
+        #'samecourse(course, course).',
+        #'sameperson(person, person).',]
+      'cora': ['sameauthor(+author,+author).',
+              'sameauthor(+author,-author).',
+              'sameauthor(-author,+author).',
+              'samebib(+class,+class).',
+              'samebib(+class,-class).',
+              'samebib(-class,+class).',
+              'sametitle(+title,+title).',
+              'sametitle(+title,-title).',
+              'sametitle(-title,+title).',
+              'samevenue(+venue,+venue).',
+              'samevenue(+venue,-venue).',
+              'samevenue(-venue,+venue).',
+              'author(+class,+author).',
+              'author(+class,-author).',
+              'author(-class,+author).',
+              'title(+class,+title).',
+              'title(+class,-title).',
+              'title(-class,+title).',
+              'venue(+class,+venue).',
+              'venue(+class,-venue).',
+              'venue(-class,+venue).',
+              'haswordauthor(+author,+word).',
+              'haswordauthor(+author,-word).',
+              'haswordauthor(-author,+word).',
+              'harswordtitle(+title,+word).',
+              'harswordtitle(+title,-word).',
+              'harswordtitle(-title,+word).',
+              'haswordvenue(+venue,+word).',
+              'haswordvenue(+venue,-word).',
+              'haswordvenue(-venue,+word).'],
+      'twitter': ['accounttype(+account,+type).',
+                  'accounttype(+account,-type).',
+                  'accounttype(-account,+type).',
+                  'tweets(+account,+word).',
+                  'tweets(+account,-word).',
+                  'tweets(-account,+word).',
+                  'follows(+account,+account).',
+                  'follows(+account,-account).',
+                  'follows(-account,+account).'],
+      'yeast': ['location(+protein,+loc).',
+                'location(+protein,-loc).',
+                'location(-protein,+loc).',
+                'interaction(+protein,+protein).',
+                'interaction(+protein,-protein).',
+                'interaction(-protein,+protein).',
+                'proteinclass(+protein,+class).',
+                'proteinclass(+protein,-class).',
+                'proteinclass(-protein,+class).',
+                'enzyme(+protein,+enz).',
+                'enzyme(+protein,-enz).',
+                'enzyme(-protein,+enz).',
+                'function(+protein,+fun).',
+                'function(+protein,-fun).',
+                'function(-protein,+fun).',
+                'complex(+protein,+com).',
+                'complex(+protein,-com).',
+                'complex(-protein,+com).',
+                'phenotype(+protein,+phe).',
+                'phenotype(+protein,-phe).',
+                'phenotype(-protein,+phe).'],
+      'nell_sports': ['athleteledsportsteam(+athlete,+sportsteam).',
+              'athleteledsportsteam(+athlete,-sportsteam).',
+              'athleteledsportsteam(-athlete,+sportsteam).',
+              'athleteplaysforteam(+athlete,+sportsteam).',
+              'athleteplaysforteam(+athlete,-sportsteam).',
+              'athleteplaysforteam(-athlete,+sportsteam).',
+              'athleteplaysinleague(+athlete,+sportsleague).',
+              'athleteplaysinleague(+athlete,-sportsleague).',
+              'athleteplaysinleague(-athlete,+sportsleague).',
+              'athleteplayssport(+athlete,+sport).',
+              'athleteplayssport(+athlete,-sport).',
+              'athleteplayssport(-athlete,+sport).',
+              'teamalsoknownas(+sportsteam,+sportsteam).',
+              'teamalsoknownas(+sportsteam,-sportsteam).',
+              'teamalsoknownas(-sportsteam,+sportsteam).',
+              'teamplaysagainstteam(+sportsteam,+sportsteam).',
+              'teamplaysagainstteam(+sportsteam,-sportsteam).',
+              'teamplaysagainstteam(-sportsteam,+sportsteam).',
+              'teamplaysinleague(+sportsteam,+sportsleague).',
+              'teamplaysinleague(+sportsteam,-sportsleague).',
+              'teamplaysinleague(-sportsteam,+sportsleague).',
+              'teamplayssport(+sportsteam,+sport).',
+              'teamplayssport(+sportsteam,-sport).',
+              'teamplayssport(-sportsteam,+sport).'],
+      'nell_finances': ['countryhascompanyoffice(+country,+company).',
+                        'countryhascompanyoffice(+country,-company).',
+                        'countryhascompanyoffice(-country,+company).',
+                        'companyeconomicsector(+company,+sector).',
+                        'companyeconomicsector(+company,-sector).',
+                        'companyeconomicsector(-company,+sector).',
+                        'companyceo(+company,+person).',
+                        'companyceo(+company,-person).',
+                        'companyceo(-company,+person).',
+                        'companyalsoknownas(+company,+company).',
+                        'companyalsoknownas(+company,-company).',
+                        'companyalsoknownas(-company,+company).',
+                        'cityhascompanyoffice(+city,+company).',
+                        'cityhascompanyoffice(+city,-company).',
+                        'cityhascompanyoffice(-city,+company).'],
+      'yago2s': ['hascurrency(+place,+currency).',
+    'hascurrency(+place,-currency).',
+    'hascurrency(-place,+currency).',
+    'hascapital(+place,+place).',
+    'hascapital(+place,-place).',
+    'hascapital(-place,+place).',
+    'hasacademicadvisor(+person,+person).',
+    'hasacademicadvisor(+person,-person).',
+    'hasacademicadvisor(-person,+person).',
+    'haswonprize(+person,+prize).',
+    'haswonprize(+person,-prize).',
+    'haswonprize(-person,+prize).',
+    'participatedin(+place,+event).',
+    'participatedin(+place,-event).',
+    'participatedin(-place,+event).',
+    'owns(+institution,+institution).',
+    'owns(+institution,-institution).',
+    'owns(-institution,+institution).',
+    'isinterestedin(+person,+concept).',
+    'isinterestedin(+person,-concept).',
+    'isinterestedin(-person,+concept).',
+    'livesin(+person,+place).',
+    'livesin(+person,-place).',
+    'livesin(-person,+place).',
+    'happenedin(+event,+place).',
+    'happenedin(+event,-place).',
+    'happenedin(-event,+place).',
+    'holdspoliticalposition(+person,+politicalposition).',
+    'holdspoliticalposition(+person,-politicalposition).',
+    'holdspoliticalposition(-person,+politicalposition).',
+    'diedin(+person,+place).',
+    'diedin(+person,-place).',
+    'diedin(-person,+place).',
+    'actedin(+person,+media).',
+    'actedin(+person,-media).',
+    'actedin(-person,+media).',
+    'iscitizenof(+person,+place).',
+    'iscitizenof(+person,-place).',
+    'iscitizenof(-person,+place).',
+    'worksat(+person,+institution).',
+    'worksat(+person,-institution).',
+    'worksat(-person,+institution).',
+    'directed(+person,+media).',
+    'directed(+person,-media).',
+    'directed(-person,+media).',
+    'dealswith(+place,+place).',
+    'dealswith(+place,-place).',
+    'dealswith(-place,+place).',
+    'wasbornin(+person,+place).',
+    'wasbornin(+person,-place).',
+    'wasbornin(-person,+place).',
+    'created(+person,+media).',
+    'created(+person,-media).',
+    'created(-person,+media).',
+    'isleaderof(+person,+place).',
+    'isleaderof(+person,-place).',
+    'isleaderof(-person,+place).',
+    'haschild(+person,+person).',
+    'haschild(+person,-person).',
+    'haschild(-person,+person).',
+    'ismarriedto(+person,+person).',
+    'ismarriedto(+person,-person).',
+    'ismarriedto(-person,+person).',
+    'imports(+person,+material).',
+    'imports(+person,-material).',
+    'imports(-person,+material).',
+    'hasmusicalrole(+person,+musicalrole).',
+    'hasmusicalrole(+person,-musicalrole).',
+    'hasmusicalrole(-person,+musicalrole).',
+    'influences(+person,+person).',
+    'influences(+person,-person).',
+    'influences(-person,+person).',
+    'isaffiliatedto(+person,+team).',
+    'isaffiliatedto(+person,-team).',
+    'isaffiliatedto(-person,+team).',
+    'isknownfor(+person,+theory).',
+    'isknownfor(+person,-theory).',
+    'isknownfor(-person,+theory).',
+    'ispoliticianof(+person,+place).',
+    'ispoliticianof(+person,-place).',
+    'ispoliticianof(-person,+place).',
+    'graduatedfrom(+person,+institution).',
+    'graduatedfrom(+person,-institution).',
+    'graduatedfrom(-person,+institution).',
+    'exports(+place,+material).',
+    'exports(+place,-material).',
+    'exports(-place,+material).',
+    'edited(+person,+media).',
+    'edited(+person,-media).',
+    'edited(-person,+media).',
+    'wrotemusicfor(+person,+media).',
+    'wrotemusicfor(+person,-media).',
+    'wrotemusicfor(-person,+media).']
+      }
+
+if os.path.isfile('transfer_experiment.json'):
+    with open('transfer_experiment.json', 'r') as fp:
+        results = json.load(fp)
+else:
+    results = { 'results': {}, 'save': { }}
+    firstRun = True
+
+def save(data):
+    with open('transfer_experiment.json', 'w') as fp:
+        json.dump(data, fp)
+        
+if firstRun:
+    results['save'] = {'experiment': 0, 'n_runs': 0, 'seed': random.randint(111111,999999) }
+
+start = time.time()
+#while results['save']['experiment'] < len(experiments):
+while results['save']['n_runs'] < n_runs:
+    experiment = results['save']['experiment'] % len(experiments)
+    try:
+        #experiment = results['save']['experiment']
+        experiment_title = experiments[experiment]['source'] + '->' + experiments[experiment]['target']
+        if experiment_title not in results['results']:
+            results['results'][experiment_title] = []
+        
+        nbr = len(results['results'][experiment_title])
+        print('Starting experiment #' + str(nbr+1) + ' for ' + experiment_title+ '\n')
+    
+        source = experiments[experiment]['source']
+        target = experiments[experiment]['target']
+        predicate = experiments[experiment]['predicate']
+        
+        # Load source dataset
+        src_total_data = datasets.load(source, bk[source], seed=results['save']['seed'])
+        src_data = datasets.load(source, bk[source], target=predicate, seed=results['save']['seed'])
+            
+        # Group and shuffle
+        src_facts = datasets.group_folds(src_data[0])
+        src_pos = datasets.group_folds(src_data[1])
+        src_neg = datasets.group_folds(src_data[2])
+                    
+        if verbose:
+            print('Start learning from source dataset\n')
+                           
+        # learning from source dataset
+        background = boostsrl.modes(bk[source], [predicate], useStdLogicVariables=False, maxTreeDepth=8, nodeSize=2, numOfClauses=8)
+        [model, total_revision_time, source_structured, will, variances] = revision.learn_model(background, boostsrl, predicate, src_pos, src_neg, src_facts, refine=None, trees=10, verbose=verbose)
+        
+        preds = mapping.get_preds(source_structured, bk[source])
+        if verbose:
+            print('Predicates from source: %s' % preds + '\n')
+            #print('Source structured tree: %s \n' % source_structured)
+        
+        # Load total target dataset
+        tar_total_data = datasets.load(target, bk[target], seed=results['save']['seed'])
+            
+        n_folds = len(tar_total_data[0])
+        results_save = []
+        for i in range(n_folds):
+            
+            print('Starting fold ' + str(i+1) + '\n')
+            
+            ob_save = {}
+            [tar_train_pos, tar_test_pos] = datasets.get_kfold_small(i, tar_total_data[0])
+            
+            # transfer
+            mapping_rules, mapping_results = mapping.get_best(preds, bk[target], datasets.group_folds(src_total_data[0]), tar_train_pos)
+            transferred_structured = transfer.transfer(source_structured, mapping_rules)
+            new_target = transfer.get_transferred_target(transferred_structured)
+            if verbose:
+                print('Best mapping found: %s \n' % mapping_rules)
+                #print('Tranferred structured tree: %s \n' % transferred_structured)
+                print('Transferred target predicate: %s \n' % new_target)
+            
+            # Load new predicate target dataset
+            tar_data = datasets.load(target, bk[target], target=new_target, seed=results['save']['seed'])
+            
+            # Group and shuffle
+            [tar_train_facts, tar_test_facts] =  datasets.get_kfold_small(i, tar_data[0])
+            [tar_train_pos, tar_test_pos] =  datasets.get_kfold_small(i, tar_data[1])
+            [tar_train_neg, tar_test_neg] =  datasets.get_kfold_small(i, tar_data[2])
+    
+            # transfer and revision theory
+            background = boostsrl.modes(bk[target], [new_target], useStdLogicVariables=False, maxTreeDepth=8, nodeSize=2, numOfClauses=8)
+            [model, t_results, structured, pl_t_results] = revision.theory_revision(background, boostsrl, target, tar_train_pos, tar_train_neg, tar_train_facts, tar_test_pos, tar_test_neg, tar_test_facts, transferred_structured, trees=10, max_revision_iterations=10, verbose=verbose)
+            t_results['Mapping results'] = mapping_results
+            t_results['Parameter Learning results'] = pl_t_results
+            ob_save['transfer'] = t_results
+            print('Dataset: %s, Fold: %s, Type: %s, Time: %s' % (experiment_title, i+1, 'transfer', time.strftime('%H:%M:%S', time.gmtime(time.time()-start))))
+            print(t_results)
+            print('\n')
+            
+            if verbose:
+                print('Start learning from scratch in target domain\n')
+            
+            # learning from scratch
+            [model, t_results, structured, will, variances] = revision.learn_test_model(background, boostsrl, new_target, tar_train_pos, tar_train_neg, tar_train_facts, tar_test_pos, tar_test_neg, tar_test_facts, trees=10, verbose=verbose)
+            ob_save['scratch'] = t_results
+            print('Dataset: %s, Fold: %s, Type: %s, Time: %s' % (experiment_title, i+1, 'scratch', time.strftime('%H:%M:%S', time.gmtime(time.time()-start))))
+            print(t_results)
+            print('\n')
+            
+            results_save.append(ob_save)
+        results['results'][experiment_title].append(results_save)
+    except Exception as e:
+        print(e)
+        print('Error in experiment of ' + experiment_title)
+        pass
+    results['save']['experiment'] += 1
+    results['save']['n_runs'] += 1
+    save(results)
