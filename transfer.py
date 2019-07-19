a0a69ec51a6adf3fd166bf8df462573621304d83
rodrigoazs <rodrigo4zevedo@gmail.com>
2019-07-18 23:10:59 -0300

    Fixed unary transfer in transfer.py

--- transfer.py
+++ transfer.py
@@ -52,7 +52,7 @@ class transfer:
     def literal_to_str(literal):
         '''Generate literal string from tuple'''
         return literal[0] + '(' + literal[1] + ')'
-        
+
     def transfer_variables(variables, from_map, to_map):
         '''Transfer variables of a clause given a mapping'''
         read = {}
@@ -65,7 +65,7 @@ class transfer:
         for i in range(len(t)):
             ret.append(read[t[i].strip()])
         return ', '.join(ret)
-        
+
     def get_mapping_struct(mapping):
         '''Generate mapping structure'''
         ret = {}
@@ -74,14 +74,14 @@ class transfer:
             if match:
                 ret[match[0][0]] = (match[1][0], match[0][1], match[1][1])
         return ret
-        
+
     def transfer_literal(literal, mapping_struct):
         if literal[0] in mapping_struct:
             m = mapping_struct[literal[0]]
             return (m[0], transfer.transfer_variables(literal[1], m[1], m[2]))
         else:
             return None
-            
+
     def get_transfer_tree_helper(path, nodes, leaves):
         children = [None, None]
         split = [] if path == '' else path.split(',')
@@ -92,15 +92,15 @@ class transfer:
         if right in nodes:
             children[1] = transfer.get_transfer_tree_helper(right, nodes, leaves)
         if left in leaves:
-            children[0] = leaves[left] # { 'type': 'leaf', 'std_dev': leaves[left][0], 'neg': leaves[left][1], 'pos': leaves[left][2] } 
+            children[0] = leaves[left] # { 'type': 'leaf', 'std_dev': leaves[left][0], 'neg': leaves[left][1], 'pos': leaves[left][2] }
         if right in leaves:
             children[1] = leaves[right]
         return { nodes[path]: children }
         # { 'type': 'node', 'literals': nodes[path], 'children': children, 'variavarc] }
-        
+
     def get_transfer_tree(nodes, leaves):
         return transfer.get_transfer_tree_helper('', nodes, leaves)
-    
+
     def add_subtree_to_false(root, subtree):
         if isinstance(root, list):
             return root
@@ -114,7 +114,7 @@ class transfer:
                 return { node_str: [true_child, subtree] }
             else:
                 return { node_str: [true_child, false_child] }
-            
+
     def merge_subtrees(left, right):
         # if TRUE node is leaf then return FALSE node
         if isinstance(left, list):
@@ -149,14 +149,14 @@ class transfer:
                 return { new_key: [true_child, false_child] }
             else:
                 # nodes with no literals should be replaced by its false subtree
-                #return false_child 
+                #return false_child
                 # nodes with no literals are replaced by its true child and merge with false child
                 return transfer.merge_subtrees(true_child, false_child)
-        
+
     def transfer_tree(tree, mapping_struct):
         ntree = copy.deepcopy(tree)
         return transfer.transfer_tree_helper(ntree, mapping_struct)
-        
+
     def get_structured_from_transfer_tree_helper(path, root, nodes, leaves):
         if isinstance(root, list):
             leaves[path] = root
@@ -170,13 +170,13 @@ class transfer:
             nodes[path] = i
             transfer.get_structured_from_transfer_tree_helper(left, children[0], nodes, leaves)
             transfer.get_structured_from_transfer_tree_helper(right, children[1], nodes, leaves)
-        
+
     def get_structured_from_transfer_tree(target, tree):
         nodes = {}
         leaves = {}
         transfer.get_structured_from_transfer_tree_helper('', tree, nodes, leaves)
         return [target, nodes, leaves]
-        
+
     def transfer(structured, mapping):
         '''Transfer structure according to mapping'''
         copied = copy.deepcopy(structured)
@@ -214,31 +214,43 @@ class transfer:
             struct[1] = new_struct[1]
             struct[2] = new_struct[2]
         return copied
-        
+
     def get_transferred_target(structured):
         '''Remove target from structured tree'''
         target = structured[0][0]
         match = re.match('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', target)
         if match:
             return match.groups()[0]
-            
+
     def get_transfer_file(source_bk, target_bk, from_pred, to_pred, recursion=False, searchArgPermutation=False, searchEmpty=False, allowSameTargetMap=False):
         srcSet = set()
+        from_arity = 0
+        to_arity = 0
         for pred in source_bk:
             a = re.sub('[\+\-\`]', '', pred)
             srcSet.add(a)
+            match = re.match('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', a)
+            if match:
+                g = match.groups()
+                if g[0] == from_pred:
+                    from_arity = len(g[1].split(','))
         tarSet = set()
         for pred in target_bk:
             a = re.sub('[\+\-\`]', '', pred)
             tarSet.add(a)
+            match = re.match('([a-zA-Z_0-9]*)\s*\(([a-zA-Z_0-9,\s]*)\)', a)
+            if match:
+                g = match.groups()
+                if g[0] == to_pred:
+                    to_arity = len(g[1].split(','))
         tra = []
         for item in srcSet:
             tra.append('source: ' + item)
         for item in tarSet:
             tra.append('target: ' + item)
-        tra.append('setMap: ' + from_pred + '(A,B)=' + to_pred + '(A,B).')
+        tra.append('setMap: ' + from_pred + '(' + ','.join([chr(65+i) for i in range(from_arity)]) + ')=' + to_pred + '(' + ','.join([chr(65+i) for i in range(to_arity)]) + ').')
         if recursion:
-            tra.append('setMap: recursive_' + from_pred + '(A,B)=recursive_' + to_pred + '(A,B).')
+            tra.append('setMap: recursion_' + from_pred + '(A,B)=recursion_' + to_pred + '(A,B).')
         tra.append('setParam: searchArgPermutation=' + str(searchArgPermutation).lower() + '.')
         tra.append('setParam: searchEmpty=' + str(searchEmpty).lower() + '.')
         tra.append('setParam: allowSameTargetMap=' + str(allowSameTargetMap).lower() + '.')
